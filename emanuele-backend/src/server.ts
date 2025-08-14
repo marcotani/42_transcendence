2c9cb09a7be8a8da69460f0bc535d72610f36469
Lele <lele@DESKTOP-R4SD94B.localdomain>
2025-08-14 23:11:27 +0200

    fixed some problems

--- emanuele-backend/src/server.ts
+++ emanuele-backend/src/server.ts
@@ -13,7 +13,10 @@ const app = Fastify({ logger: false }); // Disattiva il logger automatico di Fas
 async function buildServer() {
   // Abilita CORS per il frontend
   await app.register(cors, {
-    origin: ['http://localhost:8080', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+    ],
     credentials: true,
   });
 
